c5a14018165b02a0b2019fc69659887580ee9f9d
niezwe <niels.zweifel@uzh.ch>
2022-04-21 18:09:41 +0200

    added date to session

--- src/main/java/ch/uzh/ifi/hase/soprafs22/service/SessionService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/service/SessionService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -60,6 +61,7 @@ public class SessionService {
 
     // set host to user
     newSession.setHost(host);
+    newSession.setCreatedDate(new Date());
 
     // save to repo and flush
     newSession = sessionRepository.save(newSession);
