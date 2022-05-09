03b0dd451119cbb68c0055f768462d5a7eea482b
niezwe <niels.zweifel@uzh.ch>
2022-05-09 11:13:54 +0200

    changed sockettest

--- src/test/java/ch/uzh/ifi/hase/soprafs22/service/SocketTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/service/SocketTest.java
@@ -35,29 +35,7 @@ public class SocketTest {
 
     @BeforeEach
     public void setup() {
-        Set<ChatUser> chatListeners = new CopyOnWriteArraySet<>();
-        
-        ChatUser chatUser = new ChatUser();
-        ChatUser chatUser2 = new ChatUser();
 
-        chatUser.setUserId((long) 1);
-        chatUser.setSessionId((long) 1);
-        chatUser.setName("testName");
-        chatUser.setSocket(socket);
-
-        chatUser2.setUserId((long) 2);
-        chatUser2.setSessionId((long) 1);
-        chatUser2.setName("testName2");
-        chatUser2.setSocket(socket);
-
-        chatListeners.add(chatUser);
-        chatListeners.add(chatUser2);
-
-    }
-
-    @Test
-    public void closeWebsocketSession_success() throws IOException {
-       
     }
 }
 
