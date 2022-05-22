1af70a27c02b2b2ce932750b3e835323348c2b1c
Carol Ernst <91160233+carolernst-uzh@users.noreply.github.com>
2022-05-22 16:27:58 +0200

    Corrected typo in messageStatus

--- src/test/java/ch/uzh/ifi/hase/soprafs22/entity/MessageTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/entity/MessageTest.java
@@ -2,15 +2,9 @@ package ch.uzh.ifi.hase.soprafs22.entity;
 
 import ch.uzh.ifi.hase.soprafs22.constant.MessageType;
 import ch.uzh.ifi.hase.soprafs22.constant.SessionStatus;
-import ch.uzh.ifi.hase.soprafs22.constant.UserStatus;
-import ch.uzh.ifi.hase.soprafs22.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.web.server.ResponseStatusException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,7 +19,7 @@ public class MessageTest {
     message = new Message();
     message.setContent("testContent");
     message.setFrom("testFrom");
-    message.setMessagetype(MessageType.CommentText);
+    message.setMessageType(MessageType.CommentText);
     message.setSessionStatus(SessionStatus.ONGOING);
   }
 
@@ -33,7 +27,7 @@ public class MessageTest {
   public void createChatUser_validInputs() {
     assertEquals(message.getContent(), "testContent");
     assertEquals(message.getFrom(), "testFrom");
-    assertEquals(MessageType.CommentText, message.getMessagetype());
+    assertEquals(MessageType.CommentText, message.getMessageType());
     assertEquals(SessionStatus.ONGOING, message.getSessionStatus());
   }
 }
